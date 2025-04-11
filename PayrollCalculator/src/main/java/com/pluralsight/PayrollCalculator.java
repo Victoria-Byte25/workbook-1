f963ba3eea761399ece9eeabb5a4884cc5aa3178
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-11 12:32:07 -0500

    Updated Payroll Calculator with overtime pay

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -2,21 +2,26 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class PayrollCalculator {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your name:");
         String name = scanner.nextLine();
         System.out.print("Enter hours worked:");
         float hoursWorked = scanner.nextFloat();
         System.out.print("Enter pay rate:");
         float payRate = scanner.nextFloat();
-        float grossPay = hoursWorked * payRate;
-        System.out.println ("Employee Name:" + name);
-        System.out.println ("Gross Pay: $" + grossPay);
+        float grossPay;
+        if (hoursWorked > 40) {
+            float regularPay = 40 * payRate;
+            float overtimePay = (hoursWorked - 40) * payRate * 1.5f;
+            grossPay = regularPay + overtimePay;
+        } else {
+            grossPay = hoursWorked * payRate;
+        }
+        System.out.println("Employee Name:" + name);
+        System.out.println("Gross Pay: $" + grossPay);
 
         scanner.close();
-
-
-
     }
 }
+
