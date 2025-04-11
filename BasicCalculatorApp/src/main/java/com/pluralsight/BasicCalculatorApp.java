b6e436510bf26a8547591d55a95510bc7a085245
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-04-11 11:41:00 -0500

    Completed Mod3 -Exercise 3 -Basic Calculator App

---
+++ BasicCalculatorApp/src/main/java/com/pluralsight/BasicCalculatorApp.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class BasicCalculatorApp {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner ( System. in);
+        System.out.print("Enter the first number:");
+        float num1 = scanner.nextFloat();
+        System.out.print("Enter the second number:");
+        float num2 = scanner.nextFloat();
+        System.out.println("Possible calculations:");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultiply");
+        System.out.println("(D)ivide");
+        System.out.print("Please select an option:");
+        String option = scanner.next();
+        float result = num1 * num2;
+        System.out.println(num1 + " * " + num2 + "=" + result);
+        scanner.close();
+
+
+        //
+
+
+    }
+}
---
+++ HelloWorld/src/main/java/com/victoria/javapractice/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.victoria.javapractice;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hey Victoria");
+    }
+}
